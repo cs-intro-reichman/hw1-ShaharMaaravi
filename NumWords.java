d7594a59e2ae5b5404c736096a9a5072e6a45c90
ShaharMaaravi <shahar.maaravi@post.roni.ac.il>
2024-11-07 17:06:43 +0200

    Fix for NumWords function4

--- NumWords.java
+++ NumWords.java
@@ -4,8 +4,8 @@ public class NumWords {
 	    int input = Integer.parseInt(args[0]); 
 		int	ones = input%10;
 		int tens = (input/10)%10; 
-		int hundreds = (input/100)%10; 
-		// int thousands = (input/1000)%10; if you want to continue..  
+		int hundreds = (input/100)%100; 
+		// int thousands = (input/1000)%10; //if you want to continue..  
 
 		System.out.println(hundreds + " hundreds, " + tens + " tens, and " + ones + " ones.");
 	}
